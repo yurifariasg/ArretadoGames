200a5870069c325e59fa99f460a13f2319f95499
Guilherme Santos <guisgb13@gmail.com>
2013-03-23 16:01:46 -0300

    Initial carousel implemented

--- web/app.py
+++ web/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def index:
+def index():
 	return render_template("index.html")
 
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host="0.0.0.0", port=port)
+	app.run(host="0.0.0.0", port=port, debug=True)
