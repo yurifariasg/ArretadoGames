f379285e4748df4bd9264ac8c9d73a16a94028c6
Yuri Farias Gomes <yurifariasg@gmail.com>
2013-03-24 13:57:33 -0300

    Initial Pilot Project Commit

---
+++ PilotProject/src/com/arretadogames/pilot/physics/PhysicsWorld.java
@@ -0,0 +1,11 @@
+package com.arretadogames.pilot.physics;
+
+import org.jbox2d.dynamics.World;
+
+public class PhysicsWorld {
+	// This class should be the interface between our World and Box2D
+	
+	private World world;
+	
+
+}
---
+++ PilotProject/src/com/arretadogames/pilot/world/GameWorld.java
@@ -0,0 +1,29 @@
+package com.arretadogames.pilot.world;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.view.MotionEvent;
+
+import com.arretadogames.pilot.screens.GameScreen;
+
+/**
+ * GameWorld class represents the World in our Game
+ */
+public class GameWorld extends GameScreen {
+
+	@Override
+	public void render(GL10 gl, float timeElapsed) {
+		// TODO: Render the World
+	}
+
+	@Override
+	public void step(float timeElapsed) {
+		// TODO: Perform a World Step
+	}
+
+	@Override
+	public void input(MotionEvent event) {
+		// TODO Handle Inputs
+	}
+
+}
